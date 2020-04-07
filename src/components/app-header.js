25e8ba97b950f8cb1a3ff79badd0688cd124ec3b
Frozzesav <frozzesav@gmail.com>
2020-04-08 02:23:41 +0300

    Перенес компоненты в отдельные файлы

---
+++ src/components/app-header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppHeader = () => {
+    return <h1>Заголовок</h1>;
+  };
+
+  export default AppHeader;
---
+++ src/components/todo-list.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoList = () => {
+    return (
+        <ul>
+        <li>Дело 1</li>
+        <li>Дело 2</li>
+        <li>Дело 3</li>
+        </ul>
+    );
+};
+  
+export default TodoList;
